a1cb022590a674d966a010b5eee26b6ca61e7d65
Martynas Voldemaras <42231878+MartynasMV@users.noreply.github.com>
2021-02-10 12:33:56 +0100

    a bit more order

--- JS/experimenting.js
+++ JS/experimenting.js
@@ -348,29 +348,3 @@ button.addEventListener("mouseover", (e) => {
   document.querySelector("h1").style.background = "#ccc";
 }); //can rean about it more in MDN documentation
  */
-
-const myForm = document.querySelector("#my-form");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const message = document.querySelector(".msg");
-const userList = document.querySelector("#users2");
-
-myForm.addEventListener("submit", onSubmit);
-
-function onSubmit(e) {
-  e.preventDefault();
-  if (nameInput.value === "" || emailInput === "") {
-    message.classList.add("error");
-    message.innerHTML = "Please enter all fields";
-    setTimeout(() => message.remove(), 2000);
-  } else {
-    const li = document.createElement("li");
-    li.appendChild(
-      document.createTextNode(`${nameInput.value}: ${emailInput.value}`)
-    );
-    userList.appendChild(li);
-    //clear fields:
-    nameInput.value = "";
-    emailInput.value = "";
-  }
-}
