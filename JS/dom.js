bb6bbfbd10659715e0acbe7c4f1d3bd8910af523
Martynas Voldemaras <42231878+MartynasMV@users.noreply.github.com>
2021-02-11 15:54:40 +0100

    played with mouse events etc

--- JS/dom.js
+++ JS/dom.js
@@ -23,6 +23,7 @@ console.log(document.all[10]);
 //getElementByID not recommended to use this selector
 /* const headerTitle = document.getElementById("header-title"); //not recommended to use this selector
 console.log(headerTitle); */
+
 //SELECTORS
 //document.getElementsByClassName (same goes for getElementsByTagName)
 /* const items = document.getElementsByClassName("list-group-item");
@@ -49,14 +50,13 @@ for (var i = 0; i < items.length; i++) {
 /* const headerTitle = document.querySelector("#header-title");
 const header = document.querySelector("header"); */
 
-/* headerTitle.textContent = "New header";
-
+/* headerTitle.textContent = "New header"; 
 headerTitle.innerText = "newest chage"; */
 
 /* console.log(headerTitle.textContent); */
-//textContent wont pay attention to styling which is display none, while innerText will and output will be 'Item Lister', not 'Item lister 123'.
+//textContent wont pay attention to styling which in this case is display:none, while innerText will and output will be 'Item Lister', not 'Item lister 123'.
 
-/* headerTitle.innerHTML = "<h3>Hell</h3>"; //very interesting, it puts h3 inside h1 :O */
+/* headerTitle.innerHTML = "<h3>Hell</h3>"; //very interesting, it puts h3 INSIDE h1 :O */
 
 /* headerTitle.style.border = "2px solid black"; */
 
@@ -90,10 +90,161 @@ console.log(titles[1]);
 titles[0].textContent = "Hell";
 titles[1].textContent = "Items changed with titles[1].textContent";
  */
-const oddTitles = document.querySelectorAll("li:nth-child(odd)");
-const evenTitles = document.querySelectorAll("li:nth-child(even)");
 
+/* const oddTitles = document.querySelectorAll("li:nth-child(odd)");
+const evenTitles = document.querySelectorAll("li:nth-child(even)");
+console.log(oddTitles.length);
 for (var i = 0; i < oddTitles.length; i++) {
   oddTitles[i].style.background = "#f4f4f4";
   evenTitles[i].style.background = "#ccc";
+} */
+
+//Parents
+/* const itemList = document.querySelector("#items");
+console.log(itemList);
+//Parent Nodes : parentNode and parentElement can be used for the same
+console.log(itemList.parentNode);
+itemList.parentNode.style.background = "#f4f4f4";
+console.log(itemList.parentNode.parentNode); //parents parent
+ */
+
+/* //Children
+const itemList = document.querySelector("#items");
+console.log(itemList.childNodes); // ChildNodes - a bit tricyky as it gives not 'clean' children
+
+console.log(itemList.children); //"pure" childrent with no whitespaces as with childNode
+console.log(itemList.children[0]); //specific child
+itemList.children[0].style.fontSize = "0.7rem";
+itemList.children[1].style.background = "yellow";
+//firstChild
+console.log(itemList.firstChild); //gives us first child of ChildNode :( a bit useless..
+
+//first ElementChild
+console.log(itemList.firstElementChild); //this is 'pure''child
+itemList.firstElementChild.textContent =
+  "changed with firstElementChild.text.Content";
+
+//last Element Child
+console.log(itemList.lastChild); // as firstChild gives not 'pure' child as it is from childNode
+console.log(itemList.lastElementChild); //'pure' child
+console.log((itemList.lastElementChild.textContent = "Hay!"));
+ */
+/* 
+//Siblings
+//nextSibling and previousSibling
+const container = document.querySelector(".list-group-item");
+console.log(container.nextSibling); //'unpure' sibling... :(
+
+//nextElementSibling and previousElementSibling
+console.log(container.nextElementSibling); //selects pure next sibling
+
+//Create Element */
+/* 
+//create a div with createElement
+const newDiv = document.createElement("div");
+
+//add class
+newDiv.className = "class_created_with_className";
+//add id
+newDiv.id = "hell";
+//add atribute
+newDiv.setAttribute("title", "Hello again");
+//ad type
+newDiv.setAttribute("type", "xxx");
+
+//add text node with createTextNode
+const newDivText = document.createTextNode("Hello World text");
+
+//add text to the div
+newDiv.appendChild(newDivText);
+
+//
+const container = document.querySelector("header .container");
+const h1 = document.querySelector("header h1");
+
+container.insertBefore(newDiv, h1); //what iserting and where so newDiv inserting before h1
+
+newDiv.style.fontSize = "2rem";
+console.log(newDiv);
+ */
+
+//EVENTS!
+
+//button even listener example 1
+/* const button = document.querySelector("#button");
+button.addEventListener("click", function () {
+  console.log("Clicked");
+});
+ */
+//button event listener example 2. difference is named function
+/* const button = document.querySelector("#button");
+button.addEventListener("click", buttonClick);
+
+function buttonClick() {
+  console.log("Clicked");
+} */
+//example 3 (just as two just with more changes)
+/* 
+const button = document.querySelector("#button");
+button.addEventListener("click", buttonClick);
+
+function buttonClick() {
+  document.querySelector("#button").style.background = "red";
+  document.querySelector("#header-title").textContent = "Thank you!";
 }
+ */
+/* 
+//good trick to get all the info when I click on the button is with console.log(event)
+
+const button = document.querySelector("#button");
+button.addEventListener("click", buttonClick);
+
+function buttonClick(event) {
+  console.log(event); //to get all Mouseevents when i click
+  console.log(event.target); //to see the info about the button class id etc
+  console.log(event.target.id); //going deeper and getting buttons specific id which is called 'button'
+  console.log(event.target.className); //same as above just with class. just look at all the naming in console.log(event) and console
+  const output = document.querySelector("#output");
+  output.appendChild(document.createTextNode(event.target.id)); //or for example: output.innerHTML = "<h3>event.target.id</h3>";
+
+  //other useful things i can grab below:
+  console.log(event.type); //in this case its click
+  console.log(event.clientX); //mouse x axis on the screen (browser)
+  console.log(event.clientY); //mouse x axis on the screen (browser)
+  console.log(event.offsetX); //X of the element itself
+  console.log(event.offsetY); //Y of the element itself
+  console.log(event.shiftKey);
+  console.log(event.altKey);
+  console.log(event.ctrlKey);
+}
+ */
+/* 
+const button = document.querySelector("#button");
+button.addEventListener("click", runEvent);
+button.addEventListener("dblclick", runEvent);
+button.addEventListener("mousedown", runEvent);
+button.addEventListener("mouseup", runEvent);
+function runEvent(event) {
+  console.log("EVENT TYPE: " + event.type);
+}
+ */
+
+//mouse Enter Leave over out events
+/* 
+const button = document.querySelector("#button");
+const box = document.querySelector("#box");
+box.addEventListener("mouseenter", runEvent);
+box.addEventListener("mouseleave", runEvent);
+box.addEventListener("mouseover", runEvent);
+box.addEventListener("mouseout", runEvent);
+box.addEventListener("mousemove", runEvent);
+function runEvent(event) {
+  console.log("EVENT TYPE: " + event.type);
+  const output = document.querySelector("#output");
+  output.innerHTML =
+    "<h3>Mouse Y: " + event.offsetY + " Mouse X: " + event.offsetX + "</h3>"; //!!! need to surround even.offsetx with ++ as in this example
+  //output.innerHTML = "X: " + event.offsetX + "Y: " + event.offsetY; //simplified version of above
+
+  box.style.background = "rgb(" + event.offsetX + "," + event.offsetY + ",40)";
+}
+ */
